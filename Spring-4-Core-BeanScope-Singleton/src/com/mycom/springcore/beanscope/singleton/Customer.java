56f89212f10898bad6d1c058770eb82778bd0b8c
Ch.Divya <Ch.Divya@DESKTOP-DV8HDJT>
2021-04-06 10:50:05 +0530

    this is first commit

---
+++ Spring-4-Core-BeanScope-Singleton/src/com/mycom/springcore/beanscope/singleton/Customer.java
@@ -0,0 +1,37 @@
+package com.mycom.springcore.beanscope.singleton;
+
+public class Customer {
+int id;
+String name;
+String city;
+static int count=0;
+{//non static block
+	count++;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public static int getCount() {
+	return count;
+}
+public static void setCount(int count) {
+	Customer.count = count;
+}
+
+}
+
---
+++ Spring-4-Core-BeanScope-Singleton/src/com/mycom/springcore/beanscope/singleton/MainCustomer.java
@@ -0,0 +1,32 @@
+package com.mycom.springcore.beanscope.singleton;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainCustomer {
+public static void main(String[] args) {
+	ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+	Customer customer1=(Customer)context.getBean("customer");
+	
+	customer1.setId(1001);
+	customer1.setName("Kumar");
+	customer1.setCity("Bengaluru");
+	
+	System.out.println("Id:"+customer1.getId());
+		System.out.println("Name:"+customer1.getName());
+			System.out.println("City:"+customer1.getCity());
+				System.out.println("No of customers:"+customer1.count);
+				Customer customer2=(Customer)context.getBean("customer");
+				
+				customer2.setId(1002);
+				customer2.setName("KumarSenu");
+				customer2.setCity("Bengaluru");
+				
+				System.out.println("Id:"+customer2.getId());
+					System.out.println("Name:"+customer2.getName());
+						System.out.println("City:"+customer2.getCity());
+							System.out.println("No of customers:"+customer2.count);
+
+
+}
+}
