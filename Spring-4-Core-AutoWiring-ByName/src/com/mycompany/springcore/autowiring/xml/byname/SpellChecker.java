0990857d857fe2126bd7ad13e669ef1439c52c03
Ch.Divya <Ch.Divya@DESKTOP-DV8HDJT>
2021-04-06 10:46:50 +0530

    this is first commit

---
+++ Spring-4-Core-AutoWiring-ByName/src/com/mycompany/springcore/autowiring/xml/byname/SpellChecker.java
@@ -0,0 +1,14 @@
+package com.mycompany.springcore.autowiring.xml.byname;
+
+public class SpellChecker {
+String msg;
+
+public String getMsg() {
+	return msg;
+}
+
+public void setMsg(String msg) {
+	this.msg = msg;
+}
+
+}
---
+++ Spring-4-Core-AutoWiring-ByName/src/com/mycompany/springcore/autowiring/xml/byname/TextEditor.java
@@ -0,0 +1,21 @@
+package com.mycompany.springcore.autowiring.xml.byname;
+
+public class TextEditor {
+private SpellChecker spellChecker;
+private String name;
+public SpellChecker getSpellChecker() {
+	return spellChecker;
+}
+public void setSpellChecker(SpellChecker spellChecker) {
+	this.spellChecker = spellChecker;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public void showMessage() {
+	System.out.println(spellChecker.getMsg());
+}
+}
