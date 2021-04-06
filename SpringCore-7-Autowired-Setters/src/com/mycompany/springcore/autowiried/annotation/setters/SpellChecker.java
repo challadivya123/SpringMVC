431d5a874f6f144deca067a2657b0a78ddb96a17
Ch.Divya <Ch.Divya@DESKTOP-DV8HDJT>
2021-04-06 11:02:14 +0530

    this is first commit

---
+++ SpringCore-7-Autowired-Setters/src/com/mycompany/springcore/autowiried/annotation/setters/SpellChecker.java
@@ -0,0 +1,13 @@
+package com.mycompany.springcore.autowiried.annotation.setters;
+
+public class SpellChecker {
+	public SpellChecker() {
+		System.out.println("Inside spellchecker constructor");
+	}
+	public void checkSpelling() {
+		System.out.println("Inside checkSpelling");
+			
+	}
+	}
+
+
